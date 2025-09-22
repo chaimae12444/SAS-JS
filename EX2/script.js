86f44a117d239d4cd5060492703cae4e6783d3b1
chaimae12444 <sedraoui.chaimae.solicode@gmail.com>
2025-09-22 10:59:05 +0100

    Add files via upload

---
+++ EX2/script.js
@@ -0,0 +1,11 @@
+let note=prompt('entre une note:');
+if(note<10){
+    alert("hvvvv")
+}else{
+  if(note<=12){
+        alert("passable")
+        }
+  else{
+            alert("bien")
+        }    
+}
