6d6239014f6a86b671a69ec55b603d43ef213d3d
chaimae12444 <sedraoui.chaimae.solicode@gmail.com>
2025-09-25 11:08:56 +0100

    Add files via upload

---
+++ prototype/script.js
@@ -0,0 +1,15 @@
+while (true) {
+    let prototyp= prompte ("enter from c1 to c8")
+    switch (prototyp){
+    case "c1" :
+    alert ("Maquetter une application")
+    break;
+    case "c2" :
+        alert("Réaliser une interface utilisateur web statique et adaptable")
+        break;
+        case "c3" :
+            alert("Développer une interface utilisateur web dynamique")
+            break;
+            case "exit" :
+           alert("Cette compétence n'existe pas")
+        }}
