6b57fd5326e693392e4e60f47392d8465b9501c7
chaimae12444 <sedraoui.chaimae.solicode@gmail.com>
2025-11-05 11:01:38 +0100

    Add files via upload

---
+++ Interaction avec l’utilisateur/ex/script.js
@@ -0,0 +1,17 @@
+
+    let inpt = document.getElementById("txt");
+      let msg = document.createElement("p");
+      let list = document.querySelector("ul");
+      let btn = document.getElementById("btn");
+ btn.addEventListener("click", function () {
+        if (inpt.value.trim()) {
+          let li = document.createElement("li");
+          li.textContent = inpt.value;
+          inpt.value = "";
+          list.appendChild(li);
+        } else {
+          document.body.appendChild(msg);
+          msg.innerText = "Try Writing Somthing";
+        }
+      });
+        
